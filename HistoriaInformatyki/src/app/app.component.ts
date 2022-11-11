9cf52899cfcb5b5342c678741f09e2cced8145d1
Olcia90079 <olcia90079@wp.pl>
2022-11-11 10:18:18 +0100

    testDisplay

--- HistoriaInformatyki/src/app/app.component.ts
+++ HistoriaInformatyki/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'HistoriaInformatyki';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.tekstJakiejsTablicy()
+      .subscribe(response => console.log(response));
+  }
+
+  tekstJakiejsTablicy()
+  {
+    return this.http.get('/assets/docs/Tablice/1a')
+    .pipe(
+      switchMap((response: any) => this.http.get(response.pathToFile, {
+        responseType: 'text'
+      }))
+    );
+  }
 }
