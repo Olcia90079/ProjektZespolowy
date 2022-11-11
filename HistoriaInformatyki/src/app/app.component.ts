3adcba7b271b62865f04cfb5b2562dd0fba3fe23
Olcia90079 <olcia90079@wp.pl>
2022-11-11 12:21:33 +0100

    TekstyIAppComponent

--- HistoriaInformatyki/src/app/app.component.ts
+++ HistoriaInformatyki/src/app/app.component.ts
@@ -1,30 +1,23 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'HistoriaInformatyki';
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.tekstJakiejsTablicy()
-      .subscribe(response => console.log(response));
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get('.link/do/pliku.txt').subscribe(() => 
+    {
+      console.log('ODEBRALEM WIADOMOSC HTTP')
+    })
   }
 
-  tekstJakiejsTablicy()
+  tekstJakiejsTablicy(parametr:string)
   {
-    return this.http.get('/assets/docs/Tablice/1a')
-    .pipe(
-      switchMap((response: any) => this.http.get(response.pathToFile, {
-        responseType: 'text'
-      }))
-    );
+    
   }
 }
