3cd9cb92f9d397230864974bdfcbf34bbeec3e38
Olcia90079 <olcia90079@wp.pl>
2022-11-15 09:00:12 +0100

    dzialajace tablice

--- HistoriaInformatyki/src/app/tablice/tablice.component.ts
+++ HistoriaInformatyki/src/app/tablice/tablice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-tablice',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablice.component.css']
 })
 export class TabliceComponent implements OnInit {
+  currentBoard?: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient,private route: ActivatedRoute) {
+      const idtablicy = this.route.snapshot.params['id'];
+      console.log(idtablicy)
+    this.httpClient.get('assets/docs/Tablice/'+ idtablicy +".txt",  {
+      responseType: 'text',
+    }).subscribe(
+      (dane) => {
+        this.currentBoard = dane;
+      }
+    )
+
+   }
 
   ngOnInit(): void {
   }
