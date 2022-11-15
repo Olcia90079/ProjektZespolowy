04046efd437eca6bace754f2182cb95722988e7c
Olcia90079 <olcia90079@wp.pl>
2022-11-15 08:33:35 +0100

    obsluga wielu tablic przez ruter

--- HistoriaInformatyki/src/app/app-routing.module.ts
+++ HistoriaInformatyki/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TabliceComponent } from './tablice/tablice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tablica/:id', component: TabliceComponent
+  },
+  {
+    path: 'tablica',
+    redirectTo: 'tablica/1a',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ HistoriaInformatyki/src/app/tablice/tablice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tablice',
+  templateUrl: './tablice.component.html',
+  styleUrls: ['./tablice.component.css']
+})
+export class TabliceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
