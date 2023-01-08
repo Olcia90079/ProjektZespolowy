963c8aaed4aee832d53bb2aed10347ce5dd9ae70
Olcia90079 <olcia90079@wp.pl>
2023-01-08 14:21:27 +0100

    arrowsNavTest

--- HistoriaInformatyki/src/app/tablice/tablice.component.ts
+++ HistoriaInformatyki/src/app/tablice/tablice.component.ts
@@ -1,48 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
-import { PageEvent } from '@angular/material/paginator';
-
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tablice',
   templateUrl: './tablice.component.html',
   styleUrls: ['./tablice.component.css']
 })
+
 export class TabliceComponent implements OnInit {
-  currentBoard?: string;
-  totalPages = 10;
-  pageSize = 1;
-  pageSizeOptions = [1, 2, 5, 10];
+  
+  spisTablic: string[]; // Array to store the text files
+  currentBoard: string; // Variable to store the text file contents
+  index: number;
 
-
-  constructor(private httpClient: HttpClient,private route: ActivatedRoute) {
-      const idtablicy = this.route.snapshot.params['id'];
-      console.log(idtablicy)
-    this.httpClient.get('assets/docs/Tablice/'+ idtablicy +".txt",  {
-      responseType: 'text',
-    }).subscribe(
-      (dane) => {
-        this.currentBoard = dane;
-      }
-    )
-
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {
+    this.currentBoard = '';
+    this.spisTablic = [];
+    this.index = 0;
   }
 
-  changePage(event: PageEvent) {
-    const pageIndex = event.pageIndex + 1;
-    this.httpClient.get(`assets/docs/Tablice/${pageIndex}.txt`, {
-      responseType: 'text',
-    }).subscribe(
-      (dane) => {
-        this.currentBoard = dane;
-      }
-    );
+  ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.index = params['id'];
+      this.displayTextFile(this.index);
+    });
+
+    this.http.get('assets/docs/Tablice/SpisTablic.json').subscribe((spisTablic: Object) => {
+      this.spisTablic = spisTablic as string[];
+      const index = this.route.snapshot.params['id'];
+      this.displayTextFile(index);
+    });
   }
 
+  displayTextFile(index: number) {
+    this.http.get(`assets/docs/Tablice/${this.spisTablic[index]}.txt`, { responseType: 'text' }).subscribe(currentBoard => {
+      this.currentBoard = currentBoard;
+    });
+  }
 
-  ngOnInit(): void {
+  previousTextFile() {
+    if (this.index > 0) {
+      this.router.navigate(['/tablica', this.index - 1]);
+    } else {
+      this.router.navigate(['/tablica', this.spisTablic.length - 1]);
+    }
+  }
+
+  nextTextFile() {
+    if (this.index < this.spisTablic.length - 1) {
+      this.router.navigate(['/tablica', this.index + 1]);
+    } else {
+      this.router.navigate(['/tablica', 0]);
+    }
   }
 
 }
