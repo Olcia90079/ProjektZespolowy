86a7c0275a4aecbd5c9f440933946712f73359e4
Karolina Pomian <k.pomian.150@studms.ug.edu.pl>
2023-03-11 17:42:50 +0100

    added correct number of answers to pickRandomQuestions in quiz

--- HistoriaInformatyki/src/app/quiz/quiz.component.ts
+++ HistoriaInformatyki/src/app/quiz/quiz.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
+import { filter, map } from 'rxjs/operators';
+
 
 interface Answer {
   answer: string;
@@ -39,22 +41,37 @@ export class QuizComponent implements OnInit {
 
   private sendRequest() {
     this.http.get<QuizzQuestion[]>('assets/docs/Quiz/corrected_questions.json').subscribe(response => {
-      this.questions = this.pickRandomQuestions(3,3,response);
+      this.questions = this.pickRandomQuestions(6,2,1,[],"all",response);
     })
   }
 
-  pickRandomQuestions(numElements: number, numAnswers: number, questions: QuizzQuestion[]) {
+  pickRandomQuestions(numElements: number, numAnswers: number, numTrueAnswers: number, no: number[], page: string, questions: QuizzQuestion[]) {
     this.sharedService.setMaxScore(numElements);
     this.sharedService.setScore(0);
 
     questions.sort(() => Math.random() - 0.5);
+    questions = questions.slice(0, numElements);
 
-    for (let i = 0; i < questions.length; i++) {
-      questions[i].answers = questions[i].answers.slice(0, numAnswers)
-      questions[i].answers.sort(() => Math.random() - 0.5);
-    }
+    questions.forEach(question => {
+      const correctAnswers = question.answers.filter(answer => answer.correct).slice(0, numTrueAnswers);
+      const incorrectAnswers = question.answers.filter(answer => !answer.correct).slice(0, numAnswers - numTrueAnswers);
+      const randomizedAnswers = correctAnswers.concat(
+        incorrectAnswers
+      ).sort(() => Math.random() - 0.5);
+      question.answers = randomizedAnswers;
+    });
 
-    return questions.slice(0, numElements);
+    //for (let i = 0; i < questions.length; i++) {
+    //  questions[i].answers = Arrays.stream(questions[i].answers)
+    //    .filter(answer -> answer.correct == true)
+    //    .limit(numTrueAnswers)
+    //    .toArray(Answer[]:: new);
+
+    //  questions[i].answers = questions[i].answers.slice(0, numAnswers)
+    //  questions[i].answers.sort(() => Math.random() - 0.5);
+    //}
+
+    return questions;
   }
 
   next() {
